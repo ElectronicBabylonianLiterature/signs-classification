686dda22710ef91b4c677254cd2ca13c24d821a6
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2023-09-18 13:08:40 +0200

    script for CDP signs era retrieval

---
+++ data_retrieval/get_era.py
@@ -0,0 +1,35 @@
+from re import I
+from pymongo import MongoClient
+import sys
+import os
+import pandas
+import shutil
+sign_filter = ["GAR","KI", "NI", "U₂", "MEŠ", "KA", "TI", "TA", "ŠU", "E", "RU"]
+
+print("Enter connection string", sys.argv)
+
+client  = MongoClient(sys.argv[1])
+database = client["ebl"]
+fragments = database["fragments"]
+
+data = pandas.read_excel('csvs/corrected_instance.xlsx')
+sign_museum = data[data['sign_new'].isin(sign_filter)][['sign_new', 'museum_number','filename']]
+sign_museum_dict = sign_museum.to_dict('tight',index=False)['data']
+for idx, line in enumerate(sign_museum_dict):
+    cursor = fragments.find_one({"_id":line[1].replace("_",".")}, {"script.period" : 1})
+    if not cursor:
+        del line
+    else:   
+        if cursor['script']['period'] != "None":
+            line[1] = cursor['script']['period']
+        else:
+            del line
+print(sign_museum_dict)
+if not os.path.isdir('CDP_data'):
+    os.mkdir('CDP_data')
+for sign, era, filename in sign_museum_dict:
+    if era in ['Neo-Assyrian', 'Neo-Babylonian']:
+        if not os.path.isdir('CDP_data/' + sign + "_" + era):
+                os.mkdir('CDP_data/' + sign + "_" + era)
+        shutil.copy2(f"static/img/instance/{filename}.jpg", f'CDP_data/{sign}_{era}/{filename}.jpg')
+        
